6e214490137771aa5fc018749f79c7c33a8af782
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2022-08-31 22:30:05 -0700

    created constants file to store IP address and PORT

--- client.js
+++ client.js
@@ -1,10 +1,11 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "10.0.0.129",
-    port: "50541"
+    host: IP,
+    port: PORT,
   });
 
   conn.setEncoding("utf8");
